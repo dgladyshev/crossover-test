74e479038bcaa4298e2174bd1914d9ef4b59189b
dgladyshev <dmitry.gladishev@gmail.com>
2017-02-18 10:35:27 +0300

    initial commit

---
+++ src/main/java/com/dgladyshev/crossover/CrossoverTestApplication.java
@@ -0,0 +1,12 @@
+package com.dgladyshev.crossover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrossoverTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrossoverTestApplication.class, args);
+	}
+}
